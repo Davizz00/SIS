64ed779750b8a63e24ec9db7b91d8e4f99bd1cf3
Davizz00 <dadavivi2000@hotmail.com>
2017-10-16 00:43:51 +0200

    Add files via upload

--- public/cliente.js
+++ public/cliente.js
@@ -1,19 +1,22 @@
-const CANVAS_WIDTH= 750;
-const CANVAS_HEIGHT= 500; 
-var SOCKET_SENSOR_APAGADO = 0;
-var SOCKET_SENSOR_FINAL_DE_CARRERA = 1;
-var SOCKET_SENSOR_DHT = 2;
-var SOCKET_SENSOR_PIR = 3;
+const CANVAS_WIDTH= 750; // ancho del canvas
+const CANVAS_HEIGHT= 500;  // alto del canvas 
+const SOCKET_SENSOR_APAGADO = 0; // el sensor o sesnores no están acivados
+const SOCKET_SENSOR_FINAL_DE_CARRERA = 1;  // Valor para activar el final de carrera 
+const SOCKET_SENSOR_DHT = 2; // Valor para activar el sensor de temperatura y humedad
+const SOCKET_SENSOR_PIR = 3; // Valor para  actvar el sensor de presencia(PIR)
+var estado = SOCKET_SENSOR_APAGADO; // Asigna variable estado a la variable 
+
 var socket = io();
-const ARRAY_LENGHT = 60;
-var chart = null;
-var estado = SOCKET_SENSOR_APAGADO;
-console.log(window.innerWidth);
+
+
+const ARRAY_LENGHT = 60; 
+var chart = null; // Gráfico 
+// Configuración del gráfico de temperatura y humedad 
 var chart_config = {
     type: 'line',
     data:{
     	labels: [...Array(ARRAY_LENGHT).keys()],
-    	//labels: Array(ARRAY_LENGHT).fill(null),
+   
     	datasets:[{
     		label: "Temperatura",
 			data: Array(ARRAY_LENGHT).fill(null),
@@ -39,14 +42,14 @@ var chart_config = {
     		display: true,
     		text: 'Informacion temperatura y humedad por segundo '
     	},
-    	//tooltips: {
-    	//	mode: 'index',
-    	 //	intersect: false
-    	//},
-    	//hover: {
-    	 //	mode: 'index',
-    	 	//intersect: true
-    	//},
+    	tooltips: {
+    		mode: 'index',
+    		intersect: false
+    	},
+    	hover: {
+    		mode: 'index',
+    	 	intersect: true
+    	},
     	scales:{
     		YAxes:[{
     			display: true,
@@ -59,16 +62,19 @@ var chart_config = {
     }
 };
 
+// añadir valor de temperatura 
 function add_value_array_temperature(value){
 	chart_config.data.datasets[0].data.shift();
 	chart_config.data.datasets[0].data.push(value);
 }
 
+//Añadir valor de humedad 
 function add_value_array_humidity(value){
 	chart_config.data.datasets[1].data.shift();
 	chart_config.data.datasets[1].data.push(value);
 }
 
+// Escribir texto en el canvas 
 function dibujarcanvas_texto(texto,color){
 	var canvas = document.getElementById("myCanvas");
 	var ctx = canvas.getContext("2d");
@@ -80,26 +86,25 @@ function dibujarcanvas_texto(texto,color){
     ctx.fillText(texto, CANVAS_WIDTH/2, CANVAS_HEIGHT/2);     
 }
 
-function dibujarcanvas_imagen(img){
-	var canvas = document.getElementById("myCanvas");
-	var ctx = canvas.getContext("2d");
-	ctx.drawImage(img, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-}
+
 // Recibe la informacion del final de carrera
 socket.on('final_de_carrera', function(value){
 	estado = SOCKET_SENSOR_FINAL_DE_CARRERA; 
 	console.log(estado);
-	if (value== 0){
+	if (SOCKET_SENSOR_FINAL_DE_CARRERA == estado){
+		if (value==0){
 	  	dibujarcanvas_texto("OFF", "red");
-	}
-	else{
+		}
+		else{
 	 	dibujarcanvas_texto("ON", "green");
- 	}
+		}
+	}
  });
 
  //Recibe la informacion del sensor dht 
 socket.on("sensor_dht", function(readout){
 	estado = SOCKET_SENSOR_DHT;
+	console.log(estado);
 	if(SOCKET_SENSOR_DHT == estado){
 		add_value_array_temperature(readout.temperature);
 		add_value_array_humidity(readout.humidity);
@@ -108,28 +113,21 @@ socket.on("sensor_dht", function(readout){
 		chart.update();
 	}
 });
-//Recibe informacion del sensor pir
-//socket.on("sensor_pir", function(value){ 
-	//estado = SOCKET_SENSOR_PIR;
-	//if(value == 1){
-	//	dibujarcanvas_texto("Intruso", "red")
-	//} 
-	//else{
-	//	dibujarcanvas_texto("Sin rastro alguno", "green")	
-	//}	
- //});
-socket.on("image", function(data){
-	if(data.image){
-		console.log(data);
-		var img = new Image();
-		img.src = 'data:image/jpeg;base64,' + data.buffer;
-		dibujarcanvas_imagen(img);
+// Recibe informacion del sensor pir
+socket.on("sensor_pir", function(value){ 
+	estado = SOCKET_SENSOR_PIR;
+	console.log(estado);
+	if(value == 1){
+		dibujarcanvas_texto("Intruso", "red")
+	} 
+	else{
+		dibujarcanvas_texto("Sin rastro alguno", "green")	
 	}	
-});
+ });
 
 // funcion al hacer click en el boton sensor final de carrera
 function boton_final_de_carrera(){
-	dibujarcanvas_texto("a", "black");			 	
+	dibujarcanvas_texto("");			 	
 	socket.emit("init_sensor", SOCKET_SENSOR_FINAL_DE_CARRERA)	//Manda peticion al servidor
  }
 //funcion al hacer click en el boton temperatura
