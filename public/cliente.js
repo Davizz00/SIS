253328061dcba73a39135131b8b3e0db11837488
Davizz00 <dadavivi2000@hotmail.com>
2018-01-02 13:39:14 +0100

    Proyecto definitivo

--- public/cliente.js
+++ public/cliente.js
@@ -1,10 +1,12 @@
+// Cliente
+
 const CANVAS_WIDTH= 750; // ancho del canvas
 const CANVAS_HEIGHT= 500;  // alto del canvas 
-const SOCKET_SENSOR_APAGADO = 0; // el sensor o sesnores no están acivados
+const SOCKET_SENSOR_APAGADO = 0; // el sensor o sensores no están activados
 const SOCKET_SENSOR_FINAL_DE_CARRERA = 1;  // Valor para activar el final de carrera 
 const SOCKET_SENSOR_DHT = 2; // Valor para activar el sensor de temperatura y humedad
 const SOCKET_SENSOR_PIR = 3; // Valor para  actvar el sensor de presencia(PIR)
-var estado = SOCKET_SENSOR_APAGADO; // Asigna variable estado a la variable 
+var estado = SOCKET_SENSOR_APAGADO; // El estado del sensor está apagado (0) 
 
 var socket = io();
 
@@ -62,21 +64,21 @@ var chart_config = {
     }
 };
 
-// añadir valor de temperatura 
+// añadir valor de temperatura al gŕafico
 function add_value_array_temperature(value){
 	chart_config.data.datasets[0].data.shift();
-	chart_config.data.datasets[0].data.push(value);
+	chart_config.data.datasets[0].data.push(value); 
 }
 
-//Añadir valor de humedad 
+//Añadir valor de humedad al gŕafico
 function add_value_array_humidity(value){
-	chart_config.data.datasets[1].data.shift();
+	chart_config.data.datasets[1].data.shift(); 
 	chart_config.data.datasets[1].data.push(value);
 }
 
-// Escribir texto en el canvas 
+// Escribir texto en el canvas y su configuración
 function dibujarcanvas_texto(texto,color){
-	var canvas = document.getElementById("myCanvas");
+	var canvas = document.getElementById("myCanvas"); // Usar canvas de la página web
 	var ctx = canvas.getContext("2d");
 	ctx.fillStyle = "black";
     ctx.fillRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -91,12 +93,12 @@ function dibujarcanvas_texto(texto,color){
 socket.on('final_de_carrera', function(value){
 	estado = SOCKET_SENSOR_FINAL_DE_CARRERA; 
 	console.log(estado);
-	if (SOCKET_SENSOR_FINAL_DE_CARRERA == estado){
-		if (value==0){
-	  	dibujarcanvas_texto("OFF", "red");
+	if (SOCKET_SENSOR_FINAL_DE_CARRERA == estado){ // Compara el código del cliente con el FC
+		if (value==0){ // Si el valor es 0, escrible en el canvas OFF
+	  	dibujarcanvas_texto("OFF", "red"); // LLama a la función para escribir en el canvas
 		}
-		else{
-	 	dibujarcanvas_texto("ON", "green");
+		else{  // Si el valor es distinto a 0, obligatoriamente 1, escrible en el canvas ON
+	 	dibujarcanvas_texto("ON", "green");// LLama a la función para escribir en el canvas
 		}
 	}
  });
@@ -105,39 +107,39 @@ socket.on('final_de_carrera', function(value){
 socket.on("sensor_dht", function(readout){
 	estado = SOCKET_SENSOR_DHT;
 	console.log(estado);
-	if(SOCKET_SENSOR_DHT == estado){
-		add_value_array_temperature(readout.temperature);
-		add_value_array_humidity(readout.humidity);
+	if(SOCKET_SENSOR_DHT == estado){ // Compara el código del cliente con el FC
+		add_value_array_temperature(readout.temperature); // Función para añdir valor de temperatura
+		add_value_array_humidity(readout.humidity); // Función para añadir valor de humedad
 		if (chart == null)
 			return;
-		chart.update();
+		chart.update(); // Actualizar el gráfico
 	}
 });
 // Recibe informacion del sensor pir
 socket.on("sensor_pir", function(value){ 
 	estado = SOCKET_SENSOR_PIR;
 	console.log(estado);
-	if(value == 1){
-		dibujarcanvas_texto("Intruso", "red")
+	if(value == 1){ // Si el valor es 1, escrible en el canvas Intruso
+		dibujarcanvas_texto("Intruso", "red"); //LLama a la función para escribir en el canvas
 	} 
-	else{
-		dibujarcanvas_texto("Sin rastro alguno", "green")	
+	else{ // Si el valor es distinto de 1, escrible en el canvas Sin rastro alguno
+		dibujarcanvas_texto("Sin rastro alguno", "green"); //LLama a la función para escribir en el canvas
 	}	
  });
 
 // funcion al hacer click en el boton sensor final de carrera
 function boton_final_de_carrera(){
-	dibujarcanvas_texto("");			 	
-	socket.emit("init_sensor", SOCKET_SENSOR_FINAL_DE_CARRERA)	//Manda peticion al servidor
+	dibujarcanvas_texto(""); //LLama a la función para escribir en el canvas			 	
+	socket.emit("init_sensor", SOCKET_SENSOR_FINAL_DE_CARRERA); //Manda peticion al servidor
  }
 //funcion al hacer click en el boton temperatura
 function sensor_dht(){ 
-	socket.emit("init_sensor", SOCKET_SENSOR_DHT) // manda peticion al servidor
-	var ctx = document.getElementById('myCanvas').getContext('2d');
-	chart = new Chart(ctx, chart_config);
+	socket.emit("init_sensor", SOCKET_SENSOR_DHT); // manda peticion al servidor
+	var ctx = document.getElementById('myCanvas').getContext('2d'); // Usar canvas de la página web
+	chart = new Chart(ctx, chart_config); // Crear gŕafico 
 }
 //Funcion al hacer click boton camara
 function sensor_pir(){
-	dibujarcanvas_texto("");
-	socket.emit("init_sensor", SOCKET_SENSOR_PIR) //Manda peticion al servidor
+	dibujarcanvas_texto(""); //LLama a la función para escribir en el canvas
+	socket.emit("init_sensor", SOCKET_SENSOR_PIR); //Manda peticion al servidor
 }
--- servidor.js
+++ servidor.js
@@ -1,20 +1,25 @@
+// Servidor 
+
+// Variables para crear el servidor 
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+// Variables para utilizar los sensores 
 var Gpio = require('onoff').Gpio;
 var rpiDhtSensor = require('rpi-dht-sensor');
 
 
-var port = process.env.PORT || 8888;
+var port = process.env.PORT || 8888; // Puerto asignado 8888
 
-const SOCKET_SENSOR_APAGADO = 0;
-const SOCKET_SENSOR_FINAL_DE_CARRERA = 1;
-const SOCKET_SENSOR_DHT = 2;
-const SOCKET_SENSOR_PIR = 3;
-var estado_servidor = SOCKET_SENSOR_APAGADO; 
+const SOCKET_SENSOR_APAGADO = 0; // el sensor o sensores no están activados
+const SOCKET_SENSOR_FINAL_DE_CARRERA = 1; // Valor para activar el final de carrera 
+const SOCKET_SENSOR_DHT = 2; // Valor para activar el sensor de temperatura y humedad
+const SOCKET_SENSOR_PIR = 3; // Valor para  actvar el sensor de presencia(PIR)
+var estado_servidor = SOCKET_SENSOR_APAGADO; // El estado del sensor en el servidor está apagado (0)
 
-http.listen(port, function () {
+http.listen(port, function () { // Servidor escucha en el puerto (port 8888)
   console.log('Server listening at port %d', port);
 });
 
@@ -22,73 +27,80 @@ http.listen(port, function () {
 app.use(express.static(__dirname + '/public'));
 
 //sensores conenctados a los Gpios de la Raspberry Pi 
-var led = new Gpio(14, 'out'); 
-var final_de_carrera = new Gpio(18, 'in', 'both');
-var dht = new rpiDhtSensor.DHT11(2);
-var pir = new Gpio(21, "in", "both");
+var led = new Gpio(14, 'out'); // El led se conecta al pin GPIO 14. Información de salida
+var final_de_carrera = new Gpio(18, 'in', 'both'); // El FC se conecta al pin GPIO 18. Información de entrada y salida
+var dht = new rpiDhtSensor.DHT11(2); // El DHT se conecta al pin GPIO 2
+var pir = new Gpio(21, "in", "both");// El PIR se conecta al pin GPIO 21. Información de entrada y salida
 
 var timer_led_activado = 0;
 var timer_dht = 0;
+
 
  
 // tuneles por donde pasa la información del servidor al cliente y viceversa
 io.on('connection', function(socket){
 
-	socket.on("init_sensor", function(estado){	//recibe el estado que le manda el cliente
+	socket.on("init_sensor", function(estado){	//recibe el estado(Código) que le manda el cliente
 		estado_servidor = estado; 
 		console.log("estado servidor: ", estado_servidor);
 
-		if (estado==SOCKET_SENSOR_FINAL_DE_CARRERA){	
+		if (estado==SOCKET_SENSOR_FINAL_DE_CARRERA){ // Compara el código del cliente con el FC
 			console.log("final de carrrera");
-			clearInterval(timer_dht);  // finaliza el timer del sensor dht	
+			clear();
 		}
-		else if (estado == SOCKET_SENSOR_DHT){	
+		else if (estado == SOCKET_SENSOR_DHT){ // Compara el código del cliente con el DHT
+			clear();	
 			console.log("dht");
-			timer_dht = setInterval(read_dht, 1000);  // inicia el timer del sensor dht
-			
+			timer_dht = setInterval(read_dht, 1000);  // inicia el timer del sensor dht y llama a la función read_dht	
 		}
-		else if (estado == SOCKET_SENSOR_PIR){
-			clearInterval(timer_dht);  // Finaliza (borra) el timer del sensor dht 
+		else if (estado == SOCKET_SENSOR_PIR){ // Compara el código del cliente con el PIR
+			console.log("pir");
+			clear();
 			
 		}
 
-		final_de_carrera.watch(function(err, value){ // se ejecuta de forma asíncrona 
-			if (estado_servidor == SOCKET_SENSOR_FINAL_DE_CARRERA){
-				if (value==1){
+		final_de_carrera.watch(function(err, value){ // Lee la información del sensor. Se ejecuta de forma asíncrona 
+			if (estado_servidor == SOCKET_SENSOR_FINAL_DE_CARRERA){ // Compara el código del cliente con el del final de carrera
+				if (value==1){ 
        				value = 0;
-       				io.emit('final_de_carrera', value);  				
+       				io.emit('final_de_carrera', value); // Manda al cliente el valor 0 (No pulsado)				
   				} 
     			else{
-    			value = 1;
-    			io.emit('final_de_carrera', value);
-    				if (timer_led_activado == 0){
+    			value = 1; 
+    			io.emit('final_de_carrera', value);// Manda al cliente el valor 1 (Pulsado)	
+    				if (timer_led_activado == 0){ 
     					timer_led_activado = 1;
-   						var time = setTimeout(timer_led, 1000);
-   						led.writeSync(1); 
+   						var time = setTimeout(timer_led, 2000); // Inicializa un timer que se ejecuta a los 2 segundos y llama a la función timer_led
+   						led.writeSync(1); // Enciende el led
    						console.log("Led Encendido");						
       				}
    				} 	   		
 			}
 		});	
 
-		// Timer para que el led se apague en un tiempo determinado
+		function clear(){
+			clearInterval(timer_dht);  // Finaliza (borra) el timer del sensor dht
+		}
+
 		function timer_led(){
     		timer_led_activado = 0;
-    		led.write(0);
-    		console.log("Led Apagado")	   			
+    		led.write(0); // Se apaga el led
+    		console.log("Led Apagado");  			
  		}
 
 		function read_dht() { // se ejecuta de forma síncrona con el timer
-			var readout = dht.read();
+			if ((estado==SOCKET_SENSOR_FINAL_DE_CARRERA)||(estado == SOCKET_SENSOR_PIR)){
+				clear(); //Llama a la función clear para parar parar el sensor DHT cuando se active el PIR o el FC
+			}
+			var readout = dht.read(); // Lee  la información del sensnor DHT
 			console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' + 'humidity: ' + readout.humidity.toFixed(2) + '%'); 
-			io.emit("sensor_dht", readout);
-			timer_dht = 1s;
-					 	 			
+			io.emit("sensor_dht", readout); // Manda al cliente los calores de temperatura y humedad
+				 	 			
 		}
 
-		pir.watch(function(err, value){
-			if(estado_servidor == SOCKET_SENSOR_PIR){
-				io.emit("sensor_pir", value)	
+		pir.watch(function(err, value){ 
+			if(estado_servidor == SOCKET_SENSOR_PIR){  // Compara el código del cliente con el PIR
+				io.emit("sensor_pir", value); // Envía al cliente el valor del sensor
 			}
 		}); 	
 	});
